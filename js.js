44fdcac8c60cd50e44465c1d4b700b159489c11d
Mateja Nesic <matejanesic30@gmail.com>
2022-11-17 16:46:26 +0100

    sicNejenajjaci

--- js.js
+++ js.js
@@ -1,32 +1,32 @@
 //JavaScript radio ja. A HTML doktor Nemanja html
 
-function kreni(){
-    document.getElementById("main").innerHTML = 
-    '<div id="ned" class="polje">Ned</div><div id="pon" class="polje">Pon</div><div id="uto" class="polje">Uto</div><div id="sre" class="polje">Sre</div><div id="cet" class="polje">cet</div><div id="pet" class="polje">Pet</div><div id="sub" class="polje">Sub</div>'
- 
-    let datum = new Date(document.getElementById("godina").value, document.getElementById("mesec").value-1, 1);
-    let temp = new Date(datum.getFullYear(), datum.getMonth()+1, 0);
+function kreni() {
+    document.getElementById("main").innerHTML =
+        '<div id="ned" class="polje">Ned</div><div id="pon" class="polje">Pon</div><div id="uto" class="polje">Uto</div><div id="sre" class="polje">Sre</div><div id="cet" class="polje">Čet</div><div id="pet" class="polje">Pet</div><div id="sub" class="polje">Sub</div>'
+
+    let datum = new Date(document.getElementById("godina").value, document.getElementById("mesec").value - 1, 1);
+    let temp = new Date(datum.getFullYear(), datum.getMonth() + 1, 0);
     let dani = temp.getDate();
- 
+
     let pocetak = datum.getDay();
     console.log(datum, dani);
     let tabela = [];
     tabela.length = 42;
- 
+
     for (let i = pocetak; i < dani + pocetak; i++) {
         tabela[i] = i - pocetak + 1;
     }
     console.log(tabela)
- 
- 
+
+
     for (let i = 0; i < 42; i++) {
-        if(tabela[i] != null){
+        if (tabela[i] != null) {
             document.getElementById("main").innerHTML += '<div class="polje"><p>' + tabela[i] + '</p></div>';
         }
-        else{
+        else {
             document.getElementById("main").innerHTML += '<div class="polje"><p> </p></div>';
         }
- 
+
     }
- 
+
 }
